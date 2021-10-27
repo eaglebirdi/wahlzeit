dd5d3a2052d72ec6802ccbfced9c8b08ca198a6c
eaglebirdi <martinhelmke@gmx.de>
2021-10-27 22:07:26 +0200

    Coordinate-Methoden inkl. Tests

--- src/main/java/org/wahlzeit/model/Coordinate.java
+++ src/main/java/org/wahlzeit/model/Coordinate.java
@@ -48,4 +48,35 @@ public class Coordinate {
 		rset.updateDouble("coordinate_y", this.y);
 		rset.updateDouble("coordinate_z", this.z);
 	}
+
+	public double getDistance(Coordinate other) {
+		double diffX = this.x - other.x;
+		double diffY = this.y - other.y;
+		double diffZ = this.z - other.z;
+		double sum = diffX * diffX + diffY * diffY + diffZ * diffZ;
+		double sqrt = Math.sqrt(sum);
+		return sqrt;
+	}
+
+	public boolean isEqual(Coordinate other) {
+		final double EPSILON = 0.0001;
+		double diffX = Math.abs(this.x - other.x);
+		double diffY = Math.abs(this.y - other.y);
+		double diffZ = Math.abs(this.z - other.z);
+	
+		return diffX < EPSILON && diffY < EPSILON && diffZ < EPSILON;
+	}
+
+	public boolean equals(Object obj) {
+		if (obj == null) {
+			return false;
+		}
+
+		if (obj.getClass() != this.getClass()) {
+			return false;
+		}
+
+		Coordinate other = (Coordinate) obj;
+		return this.isEqual(other);
+	}	
 }
