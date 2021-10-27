e7e11c232493a0a90c512301d75cf4380656053b
eaglebirdi <martinhelmke@gmx.de>
2021-10-27 22:30:59 +0200

    Tests für Photo-Location

---
+++ src/test/java/org/wahlzeit/model/PhotoTest.java
@@ -0,0 +1,32 @@
+package org.wahlzeit.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+
+/**
+ * All test cases of the class {@link Location}.
+ */
+public class PhotoTest {
+	/**
+	 *
+	 */
+	@Test
+	public void testLocationIsDefaultNull() {
+		Photo photo = new Photo();
+		assertEquals(null, photo.getLocation());
+	}
+
+	/**
+	 *
+	 */
+	@Test
+	public void testLocationGetSet() {
+
+		Photo photo = new Photo();
+		Location location = new Location(new Coordinate(0.0, 0.0, 0.0));
+		photo.setLocation(location);
+		assertSame(location, photo.getLocation());
+	}
+}
