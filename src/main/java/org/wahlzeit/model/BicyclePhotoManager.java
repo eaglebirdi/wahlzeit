c2dbc237a83fddf7fd7b7efd34ee608552172831
eaglebirdi <martinhelmke@gmx.de>
2021-11-06 18:56:54 +0100

    Factory und Manager für BicyclePhoto

---
+++ src/main/java/org/wahlzeit/model/BicyclePhotoManager.java
@@ -0,0 +1,22 @@
+package org.wahlzeit.model;
+
+import java.sql.*;
+
+/**
+ * A bicycle photo manager provides access to and manages bicycle photos.
+ */
+public class BicyclePhotoManager extends PhotoManager {
+	/**
+	 * 
+	 */
+	public BicyclePhotoManager() {
+		photoTagCollector = BicyclePhotoFactory.getInstance().createPhotoTagCollector();
+	}
+
+	/**
+	 * 
+	 */
+	protected BicyclePhoto createObject(ResultSet rset) throws SQLException {
+		return BicyclePhotoFactory.getInstance().createPhoto(rset);
+	}
+}
