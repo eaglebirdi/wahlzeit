1f61eedc57b8ee7458b998c196615962885bcb2e
eaglebirdi <martinhelmke@gmx.de>
2021-11-26 20:57:26 +0100

    throws-Deklarationen in Interface

--- src/main/java/org/wahlzeit/services/Coordinate.java
+++ src/main/java/org/wahlzeit/services/Coordinate.java
@@ -3,9 +3,9 @@ package org.wahlzeit.services;
 import org.wahlzeit.model.*;
 
 public interface Coordinate {
-	CartesianCoordinate asCartesianCoordinate();
-	double getCartesianDistance(Coordinate other);
-	SphericCoordinate asSphericCoordinate();
-	double getCentralAngle(Coordinate other);
-	boolean isEqual(Coordinate other);
+	CartesianCoordinate asCartesianCoordinate() throws ArithmeticException, IllegalStateException, IllegalArgumentException;
+	double getCartesianDistance(Coordinate other) throws ArithmeticException, IllegalStateException, IllegalArgumentException;
+	SphericCoordinate asSphericCoordinate() throws ArithmeticException, IllegalStateException, IllegalArgumentException;
+	double getCentralAngle(Coordinate other) throws ArithmeticException, IllegalStateException, IllegalArgumentException;
+	boolean isEqual(Coordinate other) throws IllegalStateException, IllegalArgumentException;
 }
